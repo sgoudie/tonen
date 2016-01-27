175a8f8e410f68d7ef2e622bff70edb270b40375
Samuel Goudie <hello@samuelgoudie.co.uk>
2016-01-27 16:51:00 +0000

    First version with working sound

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,12 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext,
+      ctx = new AudioContext();
+
 App = React.createClass({
   render() {
     return (
       <div className="container">
         <h1>tönen</h1>
-        <Synth />
+        <Synth ctx={ ctx }/>
       </div>
     );
   }
--- client/components/synth.jsx
+++ client/components/synth.jsx
@@ -1,7 +1,8 @@
 Synth = React.createClass({
   getInitialState() {
     return {
-      frequency: 440
+      frequency: 440,
+      gain: 0.5
     };
   },
   handleUserInput(frequency) {
@@ -9,11 +10,26 @@ Synth = React.createClass({
       frequency: frequency
     });
   },
+  playNote(frequency, ctx) {
+    let source = ctx.createOscillator();
+    source.frequency.value = frequency;
+    source.connect(ctx.destination);
+    source.start(0);
+    source.stop(ctx.currentTime + 1);
+  },
+  componentWillMount() {
+    //Creates the audio context
+    this.masterGain = this.props.ctx.createGain();
+    this.masterGain.gain.value = this.state.gain;
+    this.masterGain.connect(this.props.ctx.destination);
+  },
   render() {
+    let self = this;
     return (
       <div className="synth">
         <h2>{this.state.frequency}hz</h2>
         <RangeSlider defaultValue={ this.state.frequency } onUserInput={ this.handleUserInput } />
+        { this.playNote(this.state.frequency, self.props.ctx) }
       </div>
     );
   }
